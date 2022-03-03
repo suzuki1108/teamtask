035f668e22945b6cf08d1a3050c267e79fc1e4b2
suzuki1108 <suzukinoko.1108@icloud.com>
2022-03-03 16:43:58 +0900

    init

---
+++ src/app/User.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function getJWTIdentifier()
+    {
+        // JWT トークンに保存する ID を返す
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        // JWT トークンに埋め込む追加の情報を返す
+        return [];
+    }
+}
