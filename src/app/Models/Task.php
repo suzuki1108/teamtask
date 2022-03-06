06a1194f39bf78b410f3029bbac1b506ff0e7738
suzuki1108 <suzukinoko.1108@icloud.com>
2022-03-06 15:46:07 +0900

    fix:bug Tasks table relation

--- src/app/Models/Task.php
+++ src/app/Models/Task.php
@@ -9,24 +9,24 @@ class Task extends Model
     // define relationships with statuses table
     public function status()
     {
-        $this->hasOne('App\Models\Status', 'id', 'status_id');
+        return $this->hasOne('App\Models\Status', 'id', 'status_id');
     }
 
     // define relationships with users table
     public function pic_user()
     {
-        $this->hasOne('App\Models\User', 'id', 'pic_user_id');
+        return $this->hasOne('App\Models\User', 'id', 'pic_user_id');
     }
 
     // define relationships with users table
     public function create_user()
     {
-        $this->hasOne('App\Models\User', 'id', 'create_user_id');
+        return $this->hasOne('App\Models\User', 'id', 'create_user_id');
     }
 
     // define relationships with teams table
     public function team()
     {
-        $this->hasOne('App\Models\Team', 'id', 'team_id');
+        return $this->hasOne('App\Models\Team', 'id', 'team_id');
     }
 }
--- src/tests/Feature/TasksTableTest.php
+++ src/tests/Feature/TasksTableTest.php
@@ -2,21 +2,58 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Task;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TasksTableTest extends TestCase
 {
+    use RefreshDatabase;
+
+    public function setup(): void
+    {
+        parent::setUp();
+        $this->artisan('migrate:fresh');
+        $this->seed('TestDataSeeder');
+    }
+
     /**
-     * A basic feature test example.
+     * タスクの管理者名が取得できること
      *
      * @return void
      */
-    public function testExample()
+    public function testGetPicUser()
     {
-        $response = $this->get('/');
+        $this->assertEquals(Task::find(1)->pic_user->name, 'test');
+    }
 
-        $response->assertStatus(200);
+    /**
+     * タスクの作成者名が取得できること
+     *
+     * @return void
+     */
+    public function testGetCreateUser()
+    {
+        $this->assertEquals(Task::find(1)->create_user->name, 'test');
+    }
+
+    /**
+     * ステータス名が取得できること
+     * 
+     * @return void
+     */
+    public function testGetStatus()
+    {
+        $this->assertEquals(Task::find(1)->status->status_name, '未着手');
+    }
+
+    /**
+     * チームが取得できること
+     * 
+     * @return void
+     */
+    public function testGetTeam()
+    {
+        $this->assertEquals(Task::find(1)->team->team_name, 'test_team');
     }
 }
